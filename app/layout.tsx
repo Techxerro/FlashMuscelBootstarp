e4c783b8a6ad92bac9286838981c8ff3de2f6d1f
Girishisglobal <girish@isglobalweb.com>
2026-02-05 19:54:17 +0530

    GTM added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,12 @@ import type { Metadata } from 'next'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/globals.css'
 import BootstrapClient from '@/components/BootstrapClient'
+import { GoogleTagManager } from '@next/third-parties/google'
+
 
 export const metadata: Metadata = {
-  title: 'Russo Pharma - Flash Muscle Supplements',
-  description: 'Unlock your peak power with medical-grade supplements. Vit2MAX and Riboxin - Advanced formulas for athletes.',
+  title: 'Flash Muscles – Best Bodybuilding & Muscle Supplements in India',
+  description: 'Shop authentic whey protein, creatine, and vitamins at FlashMuscles. We guarantee 100% original products from top global brands with lightning-fast delivery across India. Fuel your gains today!',
 }
 
 export default function RootLayout({
@@ -15,6 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <GoogleTagManager gtmId="GTM-PS8D96QK" />
       <body>
         {children}
         <BootstrapClient />
